c9fad6adb92ffe513ec406684cbaf945a6f535f9
thearise <57495459+thearise@users.noreply.github.com>
2021-10-25 15:55:21 +0630

    Updated to sps brranch

--- lib/pages2/examples.dart
+++ lib/pages2/examples.dart
@@ -1,26 +1,26 @@
-import 'dart:async';
-import 'dart:typed_data';
-
-import 'package:pdf/pdf.dart';
-
-import 'data.dart';
-import 'certificate.dart';
-
-const examples = <Example>[
-  Example('CERTIFICATE', 'certificate.dart', generateCertificate, true),
-];
-
-typedef LayoutCallbackWithData = Future<Uint8List> Function(
-    PdfPageFormat pageFormat, CustomData data);
-
-class Example {
-  const Example(this.name, this.file, this.builder, [this.needsData = false]);
-
-  final String name;
-
-  final String file;
-
-  final LayoutCallbackWithData builder;
-
-  final bool needsData;
-}
+// import 'dart:async';
+// import 'dart:typed_data';
+//
+// import 'package:pdf/pdf.dart';
+//
+// import 'data.dart';
+// import 'certificate.dart';
+//
+// const examples = <Example>[
+//   Example('CERTIFICATE', 'certificate.dart', generateCertificate, true),
+// ];
+//
+// typedef LayoutCallbackWithData = Future<Uint8List> Function(
+//     PdfPageFormat pageFormat, CustomData data);
+//
+// class Example {
+//   const Example(this.name, this.file, this.builder, [this.needsData = false]);
+//
+//   final String name;
+//
+//   final String file;
+//
+//   final LayoutCallbackWithData builder;
+//
+//   final bool needsData;
+// }
